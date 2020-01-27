2e26e212e1fae14b22dd40e7efd4371cdb514c9e
Pounat <nathan7418@gmail.com>
2020-01-27 12:18:51 -0700

    Good looking html files complete. Server looking solid. Starting my routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+app.listen(PORT, function(){
+    console.log("You are listening on http://localhost: " + PORT);
+})
