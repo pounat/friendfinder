494a6bba2d80bc6ac543c6ee7e2addbb5e73147d
Pounat <nathan7418@gmail.com>
2020-01-27 13:02:01 -0700

    Almost working file

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Jessica",
+		photo: "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/37342166_10156615696898653_4273503291427520512_n.jpg?_nc_cat=1&_nc_oc=AQmqEJA74eyuKSRsQlxWhd5pdyIR8UM8a7ZqnaYgjPg5ycKwUUAhxIJbSvRzBk6ELv5Q0hlvy6m1PzwpxAJ1vhao&_nc_ht=scontent-den4-1.xx&oh=a5bad902de0143948d4c02cd50c5905f&oe=5E97B946",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Garret",
+		photo: "https://img.huffingtonpost.com/asset/5b7ba20e1900001403502400.jpeg?cache=uv0b0oywpq&ops=scalefit_720_noupscale",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Adain",
+		photo: "https://m.media-amazon.com/images/M/MV5BNDAxMDU4OTAyN15BMl5BanBnXkFtZTgwNjM5NjczMDI@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Edith",
+		photo: "https://i.insider.com/59552bbb298d4ae20d8b479e?width=800&format=jpeg&auto=webp",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Beatrice",
+		photo: "https://www.rd.com/wp-content/uploads/2018/12/Princess-Beatrice-1-778x1024.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 1, 1, 3]
+	}
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,6 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
     console.log(req.body.scores);
 
-    // Receive user details (name, photo, scores)
     var user = req.body;
 
     // parseInt for scores
@@ -21,8 +20,7 @@ module.exports = function(app) {
     var bestFriendIndex = 0;
     var minimumDifference = 40;
 
-    // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
-    //  whatever the difference is, add to the total difference
+    //Adding up differnce
     for(var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
       for(var j = 0; j < friends[i].scores.length; j++) {
@@ -30,7 +28,7 @@ module.exports = function(app) {
         totalDifference += difference;
       }
 
-      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+      // if there is a new minimum, change the best friend index and set the new minimum for next iteration
       if(totalDifference < minimumDifference) {
         bestFriendIndex = i;
         minimumDifference = totalDifference;
